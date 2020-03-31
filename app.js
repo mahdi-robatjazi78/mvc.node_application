63021215cd44cb1c5f0420bd368eda5206acaa7f
mahdi robatjazi <realmehdi1999m@gmail.com>
2020-03-31 06:18:02 -0700

    FEATURE : ADD MORGAN LOGGER TO THIS PROJECT

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const appRouter = require("./routes")
+const path = require('path')
+const morgan = require('morgan')
+const fs = require('fs')
+const accessLogStream = fs.createWriteStream(path.join(__dirname,'log/logger.log'))
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname+"/public"))
+app.use(morgan('combined',{stream:accessLogStream}))
 app.use(appRouter)
 
-
-
 app.listen(3000,()=>{
-    console.log('server is running on port 3000');
+    console.log(`server is running on port 3000`)
 })
