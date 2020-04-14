6845fa86dab41e95835d61734bfeab132c3defdc
mahdi robatjazi <realmehdi1999m@gmail.com>
2020-04-14 16:33:03 +0330

    #feauture : ADD SIGN UP CONTROLLER FOR SEE EXSITING DATA IN DATABASE

--- controller/userController.js
+++ controller/userController.js
@@ -6,29 +6,66 @@ let date = d.getFullYear() + '/' + d.getMonth() + '/' + d.getDate()
 let time = d.getHours() +':'+d.getMinutes()+":"+d.getSeconds()
 
 
+const signUpControl = async(userName,phone,email) =>{
+    try {
+        let checkUserName  = await userModel.findOne({userName})
+        let checkUserPhone = await userModel.findOne({phone})
+        let checkUserEmail = await userModel.findOne({email})
+
+        if(checkUserEmail){
+            console.log('your user Email already been saved');
+            return true
+        }
+        else if(checkUserPhone){
+            console.log('your user Phone already been saved');
+            return true
+        }
+        else if(checkUserName){
+            console.log('your user Name already been saved');
+            return true
+        }else{
+            return false
+        }
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 const controller = {
     signUp:async(req,res)=>{
         try{
             let {userName,phone,email,password} = req.body
-            let salt = await bcrypt.genSalt(10)
-            let hash = await bcrypt.hash(password,salt)
-            password = hash
-            
-            const newPerson = new userModel({
-                userName,
-                phone,
-                email,
-                password ,
-                signUpDate : date,
-                signUpTime : time
-            })
-            await newPerson.save(()=>{
-                res.end()
-            })
+
+            let existInDataBase = await signUpControl(userName,phone,email)
+            if (existInDataBase == true){
+                await res.status(400).send('your data exist in database please set another data')
+                return
+
+            }else{
+                let salt = await bcrypt.genSalt(10)
+                let hash = await bcrypt.hash(password,salt)
+                
+                const newPerson = new userModel({
+                    userName,
+                    phone,
+                    email,
+                    password:hash,
+                    signUpDate : date,
+                    signUpTime : time
+                })
+                await newPerson.save(()=>{
+                    res.status(200).send('person successfully added to database')
+                })  
+            }
         }catch(err){
             console.error(err);
         }
     },
+
+
+
     fetchAllData:async(req,res)=>{
         try {
             let data = await userModel.find({})
--- public/js/UserScript.js
+++ public/js/UserScript.js
@@ -1,49 +1,58 @@
 $(document).ready(function () {
+
   const fetch_Users_Info = () => {
+
     $.get("/userList/fetch", function({data,count}) {
       var table = new Vue({
         el: "#table",
         data: {
           people: data.reverse(),
-          count,  
+          count,
+          textSearch:null,  
         },
         methods: {
-          //remove user
+          //remove Users
           RemoveUser: function(user) {
             const result = confirm("you sure ?? you creazy???");
             result
             ? $.ajax({
-              url: "/userList/removeUser",
-              method: "delete",
-              data: user
-            })
-            .done(function() {
-              alert(`${user.userName}  has deleted now`);
-              window.location.reload();
-            })
-            .fail(function(err) {
-              console.error(err);
-            })
+                url: "/userList/removeUser",
+                method: "delete",
+                data: user
+              })
+              .done(function() {
+                alert(`${user.userName}  has deleted now`);
+                window.location.reload();
+              })
+              .fail(function(err) {
+                console.error(err);
+              })
             : () => {
-              return;
-            };
+                return;
+              };
           },
           // begin update
           beginUpdate: function(user) {
             form.userName = user.userName;
             form.phone = user.phone
             form.email = user.email;
-            form.password = user.password;
             form._id = user._id
+          },
+          //search Users
+          searchUser:function(){
+            let user = data.filter(item=>item.userName.startsWith(this.textSearch))
+            this.people = user
           }
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
 
   fetch_Users_Info();
   
+
+
   var form = new Vue({
     el: "#form",
     data:{
@@ -59,7 +68,6 @@ $(document).ready(function () {
           userName: this.userName,
           phone:this.phone,
           email: this.email,
-          password: this.password,
           _id: this._id
         }
 
@@ -80,13 +88,19 @@ $(document).ready(function () {
           userName: this.userName,
           phone:this.phone,
           email: this.email,
-          password: this.password,
+          password:this.password
         };
 
         // SIGNUP NEW USER
-        $.post("/userList/signUp", info, () => {
-          window.location.reload();
-        });
+        $.ajax({
+          method:"post",
+          url:"/userList/signUp",
+          data:info,
+        }).done(function(msg){
+          console.log(msg);
+        }).fail(function(msg){
+          console.log(msg);
+        })
       }
     }
   })
